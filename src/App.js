c676b8faade748fc564c470a61da09406afde7b4
rimmmming <jrim1307@gmail.com>
2019-03-25 21:49:14 +0900

    phone form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,90 @@
 import React, { Component } from 'react';
-import MyName from './MyName';
-import './App.css';
+import PhoneForm from './component/phoneForm';
+import PhoneInfoList from './component/PhoneInfoList';
 
 class App extends Component {
-  render() {
-    return (
-      <MyName name="리액트" />
-    );
-  }
+
+	id = 3;
+	state = {
+		information : [
+			{
+				id : 0,
+				name:"홍길동",
+				phone : "0123-2345-346",
+			},
+			{
+				id : 1,
+				name:"박길동",
+				phone : "0123-2345-346",
+			},
+			{
+				id : 2,
+				name:"김길동",
+				phone : "0123-2345-346",
+			},
+			
+		],
+		keyword : '',
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			keyword : e.target.value,
+		})
+	}
+
+	handleCreate = (data) => {
+		const { information } = this.state;
+		this.setState({
+			information : information.concat( Object.assign( {}, data, {
+				id : this.id++
+			}))
+		});
+	}
+
+	handleRemove = (id) => {
+		const { information } = this.state;
+		this.setState({
+			information : information.filter(info => info.id !== id)
+		});
+	}
+
+	handleUpdate = (id, data) => {
+		const { information } = this.state;
+		this.setState({
+			information : information.map(
+				info => {
+					if(info.id === id){
+						return {
+							id, 
+							...data,
+						}
+					}
+					return info;
+				}
+			)
+		})
+	}
+
+	render() {
+		return (
+		<div className="App">
+			<PhoneForm  onCreate={this.handleCreate} />
+			<input 
+				value = {this.state.keyword}
+				onChange = {this.handleChange}
+				placeholder = "..검색"
+			/>
+			<PhoneInfoList 
+				data={this.state.information.filter(
+					info => info.name.indexOf(this.state.keyword) > -1
+				)}
+				onRemove={this.handleRemove}
+				onUpdate={this.handleUpdate}
+			/>
+		</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/component/phoneInfo.js
@@ -0,0 +1,93 @@
+import React, { Component, Fragment } from 'react';
+
+class PhoneInfo extends Component {
+
+    state = {
+        editing : false,
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if(this.state !== nextState){
+            return true;
+        }
+        return this.props.info !== nextProps.info;
+    }
+    
+
+    handleRemove = () => {
+        const { info, onRemove } = this.props;
+        onRemove(info.id);
+    }
+
+    handleToggleEdit = () => {
+        const { info, onUpdate } = this.props;
+        if(this.state.editing){
+            onUpdate(info.id, {
+                name : this.state.name,
+                phone : this.state.phone
+            });
+        } else {
+            this.setState({
+                name : info.name,
+                phone : info.phone,
+            });
+        }
+        this.setState({
+            editing : !this.state.editing,
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    render() {
+        const { name, phone } = this.props.info;
+
+        console.log(name)
+
+        const { editing } = this.state;
+
+        const style = {
+            border : '1px solid black',
+            padding: '8px',
+            margin : '8px',
+        };
+
+        return (
+            <div style={style}>
+                {
+                    editing ? (
+                        <Fragment>
+                            <div><input 
+                                name="name"
+                                onChange={this.handleChange} 
+                                value={this.state.name}
+                            /></div>
+                            <div><input 
+                                name="phone"
+                                onChange={this.handleChange} 
+                                value={this.state.phone}
+                            /></div>
+                        </Fragment>
+                    ):(
+                        <Fragment>
+                            <div><b>{name}</b></div>
+                            <div>{phone}</div>
+                        </Fragment>
+                    )
+                }
+                <button type="button" onClick={this.handleRemove}>삭제</button>
+                <button type="button" onClick={this.handleToggleEdit}>
+                    {
+                        editing ? "적용" : "수정"
+                    }
+                </button>
+            </div>
+        );
+    }
+}
+
+export default PhoneInfo;
